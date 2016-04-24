ee6792c6411c1203a14eb6f3f935c2f5927ccbb6
Anders Fisher <anders@devmountain.co.uk>
2016-04-24 10:10:28 +0100

    Add a test module to show how require and module exports works

---
+++ js/example.js
@@ -0,0 +1,5 @@
+module.exports = {
+  exclaim: function(message){
+    return "!!!" + message + "!!!";
+  }
+}
--- js/init.js
+++ js/init.js
@@ -1,1 +1,2 @@
-console.log('hello world!');
+var example = require('./example.js');
+console.log(example.exclaim('hello world'));
