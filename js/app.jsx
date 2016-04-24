eb2e9d0fbc8d3aa3d8bdac3574a1bc3ad217e49a
Anders Fisher <anders@devmountain.co.uk>
2016-04-24 16:23:57 +0100

    Create graph component of last N points recorded

--- js/app.jsx
+++ js/app.jsx
@@ -5,18 +5,19 @@ var Output = require('./output');
 module.exports = React.createClass({
   displayName: 'App',
   getInitialState: function(){
-    return({ x: 0, y: 0});
+    return({ x: 0, y: 0, points:[]});
   },
   move: function(x, y){
     this.setState({
       x: x,
-      y: y
+      y: y,
+      points: this.state.points.concat([{x: x, y: y}])
     });
   },
   render: function(){
     return(<div className="container">
       <Input onMove={this.move} />
-      <Output x={this.state.x} y={this.state.y} />
+      <Output x={this.state.x} y={this.state.y} points={this.state.points} />
     </div>);
   }
 });
--- js/output.jsx
+++ js/output.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
-
+var Graph = require('./graph');
 module.exports = React.createClass({
   displayName: 'Output',
   render: function(){
     return(
       <div className="output">
-        <p>{this.props.x}</p>
-        <p>{this.props.y}</p>
+        <p>x: {this.props.x}</p>
+        <p>y: {this.props.y}</p>
+        <p>count: {this.props.points.length}</p>
+        <Graph points={this.props.points} limit="200" width="300" height="300" />
+        <Graph points={this.props.points} limit="50" width="100" height="100" />
       </div>
     );
   }
